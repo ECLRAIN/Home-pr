c41cdb99f62852ea4e4510deae2ab73d4db7880a
irlaw <irlaw@180.71.94.151>
2022-07-31 21:50:46 +0900

    pr1

--- src/day01/Pr1.java
+++ src/day01/Pr1.java
@@ -2,8 +2,13 @@ package day01;
 
 public class Pr1 {
 	public static void main(String[] args) {
+		//준비 자장면 5천원, 짬뽕 6천원일때 자장면 2그릇 짬뽕 3그릇 가격
+		int jjajang = 5000; int jambbong=6000;
 		
+		//계산
+		int total=jjajang*2+jambbong*2;
+		//출력
+		System.out.println(total+"원");
 	}
-
 }
 
