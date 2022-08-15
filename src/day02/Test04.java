ede49250a670e0441f4412617199b0ff4d3a137c
irlaw <irlaw@DESKTOP-PSE3G1V>
2022-08-15 19:11:26 +0900

    복습

--- src/day02/Test04.java
+++ src/day02/Test04.java
@@ -2,8 +2,8 @@ package day02;
 
 public class Test04 {
 	public static void name(String [] args) {
-		int adult=12000;
-		int kid= 7500;
+		int adult = 12000;
+		int kid = 7500;
 		
 		System.out.println(adult);
 	}
